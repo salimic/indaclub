62054634b5540dff47796a3e3d1bfcc2775817ea
salimic <salim@kadastr05.ru>
2019-01-22 18:17:57 +0300

    1save

---
+++ app/src/main/java/com/example/salimic/watchclub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.salimic.watchclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
